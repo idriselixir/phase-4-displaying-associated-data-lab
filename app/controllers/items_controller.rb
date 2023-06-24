05a72d9db529c47883bf4b240287de104f52eb1c
Idris Noor <idreesnoor150@gmail.com>
2023-06-25 02:56:15 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,29 @@
+# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
+  def index
+    items = Item.all
+    render json: items_with_user(items)
+  end
+
+  private
+
+  def items_with_user(items)
+    items.map do |item|
+      {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        price: item.price,
+        user: user_details(item.user)
+      }
+    end
+  end
+
+  def user_details(user)
+    {
+      id: user.id,
+      username: user.username,
+      city: user.city
+    }
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
+# app/controllers/users_controller.rb
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    render json: user_with_items(user)
+  end
+
+  private
+
+  def user_with_items(user)
+    {
+      id: user.id,
+      username: user.username,
+      city: user.city,
+      items: user.items.map { |item| item_details(item) }
+    }
+  end
+
+  def item_details(item)
+    {
+      id: item.id,
+      name: item.name,
+      description: item.description,
+      price: item.price
+    }
+  end
 end
